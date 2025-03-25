81c10273f1f17024ba3da04f09470816afead13d
sheabali <iamskpranto@gmail.com>
2025-03-25 17:39:12 +0600

    create user

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import cors from 'cors';
 import express, { Application, Request, Response } from 'express';
+import router from './app/routes';
 
 const app: Application = express();
 
@@ -8,7 +9,7 @@ app.use(express.json());
 app.use(cors({ origin: ['http://localhost:5173'] }));
 
 // application routes
-// app.use('/api/v1', router);
+app.use('/api/v1', router);
 
 const test = async (req: Request, res: Response) => {
   Promise.reject();
